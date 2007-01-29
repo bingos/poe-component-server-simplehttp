14965b70394b593be696e19de48674f8c9231363
Chris Williams <chris@bingosnet.co.uk>
2007-01-29 15:07:26 +0000

    Kwalitee fixes

--- lib/POE/Component/Server/SimpleHTTP.pm
+++ lib/POE/Component/Server/SimpleHTTP.pm
@@ -7,7 +7,7 @@ use warnings FATAL => 'all';				# Enable warnings to catch errors
 
 # Initialize our version
 # $Revision: 1181 $
-our $VERSION = '1.15';
+our $VERSION = '1.16';
 
 # Import what we need from the POE namespace
 use POE;
