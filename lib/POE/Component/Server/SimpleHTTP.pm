163155efea3141ca14e974f956c961a0c642f563
Chris Williams <chris@bingosnet.co.uk>
2007-03-15 19:35:12 +0000

    Bumped the version number for release

--- lib/POE/Component/Server/SimpleHTTP.pm
+++ lib/POE/Component/Server/SimpleHTTP.pm
@@ -7,7 +7,7 @@ use warnings FATAL => 'all';				# Enable warnings to catch errors
 
 # Initialize our version
 # $Revision: 1181 $
-our $VERSION = '1.20';
+our $VERSION = '1.21';
 
 # Import what we need from the POE namespace
 use POE;
